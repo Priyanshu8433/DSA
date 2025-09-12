11b6a0d218479ae2192392b3ee690e1c65395939
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2025-09-12 19:00:31 +0530

    Time: 3 ms (99.28%) | Memory: 59.9 MB (45.51%) - LeetSync

--- 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.cpp
+++ 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     int minOperations(string s) {
         int mChar=123;
-        for(auto ch: s) if(ch!='a') mChar=min(mChar,(int)ch);
+        for(auto ch: s){
+            if(ch!='a') mChar=min(mChar,(int)ch);
+            if(ch=='b') break;
+        }
         return 123-mChar;
     }
 };
