491b0da172477fb5ac0a9be20c706059b6ddb63d
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2025-09-29 17:36:04 +0530

    Time: 23 ms (88.36%) | Memory: 106.6 MB (76.42%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
         sort(asteroids.begin(), asteroids.end());
-
         int n=asteroids.size();
 
         for(int i=0; i<n; i++){
-            if(mass>=INT_MAX-asteroids[i]) return true;
+            if((mass>=INT_MAX-asteroids[i]) || (mass>=asteroids[n-1])) return true;
             if(mass<asteroids[i]) return false;
             if(mass>=asteroids[n-1]-asteroids[i]) return true;
             if(mass>=asteroids[i]) mass+=asteroids[i];
