53f2950ec889afba1ef317d5e713b31ad240b73f
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2025-10-17 17:05:01 +0530

    Time: 0 ms (100.00%) | Memory: 36.5 MB (96.49%) - LeetSync

---
+++ 2654-count-the-number-of-vowel-strings-in-range/count-the-number-of-vowel-strings-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isVowel(char ch){
+        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
+    }
+    int vowelStrings(vector<string>& words, int left, int right) {
+        int cnt=0;
+        for(int i=left; i<=right; i++){
+            if(isVowel(words[i].front()) && isVowel(words[i].back())) cnt++;
+        }
+        return cnt;
+    }
+};
