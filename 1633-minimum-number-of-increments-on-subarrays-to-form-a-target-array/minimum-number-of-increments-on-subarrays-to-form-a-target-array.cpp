a118d5a10609e0c48a0bc41b7a3ba3f6929744de
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2025-10-30 12:09:13 +0530

    Time: 0 ms (100.00%) | Memory: 76.9 MB (65.87%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -4,9 +4,7 @@ public:
         int prev=0;
         int cnt=0;
         for(int x: target){
-            if(x>prev){
-                cnt+=(x-prev);
-            }
+            if(x>prev) cnt+=(x-prev);
             prev=x;
         }
         return cnt;
