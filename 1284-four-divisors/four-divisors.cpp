c947a0a4ebe2e8e15fe2c92a1326105b8ce6430b
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2026-01-04 11:42:29 +0530

    Time: 9 ms (81.53%) | Memory: 16.3 MB (32.76%) - LeetSync

--- 1284-four-divisors/four-divisors.cpp
+++ 1284-four-divisors/four-divisors.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
+    int solve(int n){
+        int cnt=0;
+        int sum=0;
+        // cout<<"For:"<<n<<endl;
+        for(int i=1; i*i<=n; i++){
+            if(n%i==0){
+                int sec=n/i;
+                if(sec==i){
+                    cnt+=1;
+                    sum+=i;
+                }
+                else{
+                    cnt+=2;
+                    sum+=i+sec;
+                }
+            }
+            if(cnt>4) return 0;
+        }
+        if(cnt==4){
+            return sum;
+        }
+        return 0;
+    }
+
     int sumFourDivisors(vector<int>& nums) {
         int sum=0;
         for(int num: nums){
-            int cnt=0;
-            int divSum=0;
-            for(int x=1; x<=num; x++){
-                if(num%x==0) {
-                    divSum+=x;
-                    cnt++;
-                }
-                if(cnt>4) break;
-            }
-            if(cnt==4) sum+=divSum;
+            sum+=solve(num);
         }
         return sum;
     }
