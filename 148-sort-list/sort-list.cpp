ac293687bb27e9604cb89277a0c1828a66f7c9a7
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2026-01-21 21:00:35 +0530

    Time: 47 ms (35.53%) | Memory: 75.6 MB (41.19%) - LeetSync

---
+++ 148-sort-list/sort-list.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findMiddle(ListNode* head){
+        ListNode* fast=head,*slow=head;
+
+        while(fast && fast->next){
+            fast=fast->next;
+
+            if(fast->next){
+                slow=slow->next;
+                fast=fast->next;
+            }
+        }
+        return slow;
+    }
+
+    ListNode* merge(ListNode* head1, ListNode* head2){
+        ListNode* dummy=new ListNode(-1);
+        ListNode *itr1=head1, *itr2=head2, *temp=dummy;
+        while(itr1 && itr2){
+            if(itr1->val<itr2->val){
+                temp->next=itr1;
+                temp=itr1;
+                itr1=itr1->next;
+            }
+            else{
+                temp->next=itr2;
+                temp=itr2;
+                itr2=itr2->next;
+            }
+        }
+
+        if(itr1) temp->next=itr1;
+        else temp->next=itr2;
+
+        return dummy->next;
+    }
+
+    ListNode* mergesort(ListNode* head){
+        if(!head || !head->next) return head;
+
+        ListNode* middle=findMiddle(head);
+        ListNode* next=middle->next;
+        middle->next=NULL;
+        head=mergesort(head);
+        next=mergesort(next);
+        return merge(head,next);
+    }
+
+    ListNode* sortList(ListNode* head) {
+        // ListNode* last=head;
+        // while(last->next) last=lasy->next;
+        return mergesort(head);
+    }
+};
