929089db1e760a28704ff6109bb112e011311fec
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2026-01-30 14:24:03 +0530

    Time: 11 ms (13.64%) | Memory: 19.1 MB (22.93%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,49 +1,37 @@
 class Solution {
-    bool dfs(int node, vector<vector<int>> &adjList, vector<int> &vis, vector<int> &pathVis, stack<int> &s){
-        vis[node]=1;
-        pathVis[node]=1;
-
-        for(int x: adjList[node]){
-            if(!vis[x]){
-                if(dfs(x,adjList,vis,pathVis,s)) return true;
-            }
-            else{
-                if(pathVis[x]) return true;
-            }
-        }
-        s.push(node);
-        pathVis[node]=0;
-        return false;
-    }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        /*Algorithm*/
-        /*
-         Topological order and if there's a cycle then it's not possible
-        */
-
-        stack<int> s;
         vector<vector<int>> adjList(numCourses);
-        vector<int> vis(numCourses,0);
-        vector<int> pathVis(numCourses,0);
 
-        for(auto vec: prerequisites){
-            adjList[vec[1]].push_back(vec[0]);
+        for(auto v: prerequisites){
+            adjList[v[1]].push_back(v[0]);
         }
 
+        vector<int> inDegree(numCourses,0);
+
         for(int i=0; i<numCourses; i++){
-            if(!vis[i]){
-                if(dfs(i,adjList,vis,pathVis,s)) return {};
+            for(int it: adjList[i]) inDegree[it]++;
+        }
+
+        queue<int> q;
+        vector<int> ans;
+
+        for(int i=0; i<numCourses; i++){
+            if(inDegree[i]==0) q.push(i);
+        }
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it: adjList[node]){
+                inDegree[it]--;
+                if(inDegree[it]==0) q.push(it);
             }
         }
 
-        vector<int> res;
-        while(!s.empty()){
-            int x=s.top();
-            s.pop();
-            res.push_back(x);
-        }
-
-        return res;
+        if(numCourses!=ans.size()) return {};
+        return ans;
     }
 };
