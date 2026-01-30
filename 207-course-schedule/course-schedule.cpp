7d5b7be503aa4b67dd6d1e0d5713730124d8f9c2
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2026-01-30 14:14:02 +0530

    Time: 5 ms (56.92%) | Memory: 20 MB (36.41%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,36 +1,33 @@
 class Solution {
-    bool dfs(int node, vector<vector<int>> &adjList, vector<int> &vis, vector<int> &pathVis){
-        vis[node]=1;
-        pathVis[node]=1;
-
-        for(int x: adjList[node]){
-            if(!vis[x]){
-                if(dfs(x,adjList,vis,pathVis)) return true;
-            }
-            else{
-                if(pathVis[x]) return true;
-            }
-        }
-
-        pathVis[node]=0;
-        return false;
-    }
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adjList(numCourses);
+    bool canFinish(int numCourses, vector<vector<int>>& prereq) {
+        int n=numCourses;
+        vector<vector<int>> adjList(n);
 
-        for(auto vec: prerequisites){
+        for(auto vec: prereq){
             adjList[vec[0]].push_back(vec[1]);
         }
+        vector<int> inDegree(n,0);
+        for(int i=0; i<n; i++){
+            for(auto it: adjList[i]) inDegree[it]++;
+        }
 
-        vector<int> vis(numCourses,0);
-        vector<int> pathVis(numCourses,0);
+        queue<int> q;
 
-        for(int i=0; i<numCourses; i++){
-            if(!vis[i]){
-                if(dfs(i,adjList,vis,pathVis)) return false;
+        for(int i=0; i<n; i++){
+            if(inDegree[i]==0) q.push(i);
+        }
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            n--;
+            for(auto it: adjList[node]){
+                inDegree[it]--;
+                if(inDegree[it]==0) q.push(it);
             }
         }
-        return true;
+
+        return n==0;
     }
 };
