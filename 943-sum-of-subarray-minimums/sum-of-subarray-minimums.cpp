d660f3ebd950329ad08cc687a94cdbeb22d26f52
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2026-02-08 19:39:17 +0530

    Time: 16 ms (58.91%) | Memory: 47 MB (77.75%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,41 +1,42 @@
-#define MOD 1000000007
-
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
+        int MOD=1e9+7;
         int n=arr.size();
-        // Next smaller element
         vector<int> nse(n);
+        vector<int> pse(n);
         stack<int> s;
-        for(int i=n-1; i>=0; i--){
-            while(!s.empty() && arr[s.top()]>arr[i]) s.pop();
-
-            if(s.empty()) nse[i]=n;
-            else nse[i]=s.top();
-
+        for(int i=0; i<n; i++){
+            if(s.empty()){
+                pse[i]=-1;
+            }
+            else{
+                while(!s.empty() && arr[s.top()]>=arr[i]) s.pop();
+                if(s.empty()) pse[i]=-1;
+                else pse[i]=s.top();
+            }
             s.push(i);
         }
-
         while(!s.empty()) s.pop();
 
-        //Previous smaller element
-        vector<int> pse(n);
-        for(int i=0; i<n; i++){
-            while(!s.empty() && arr[s.top()]>=arr[i]) s.pop();
-
-            if(s.empty()) pse[i]=-1;
-            else pse[i]=s.top();
-
+        for(int i=n-1; i>=0; i--){
+            if(s.empty()){
+                nse[i]=n;
+            }
+            else{
+                while(!s.empty() && arr[s.top()]>arr[i]) s.pop();
+                if(s.empty()) nse[i]=n;
+                else nse[i]=s.top();
+            }
             s.push(i);
         }
 
-        //count the number of subarrays
-        long long total=0;
+        int sum=0;
+
         for(int i=0; i<n; i++){
-            int right=nse[i]-i;
-            int left=i-pse[i];
-            total=(total+(left*right*1ll*arr[i])%(int)MOD)%(int)MOD;
+            int subArr=((nse[i]-i)%MOD*(i-pse[i])%MOD)%MOD;
+            sum=sum%MOD+(1LL*subArr%MOD*arr[i]%MOD)%MOD;
         }
-        return total;
+        return sum;
     }
 };
