7b284fd2be2cb721bb404ba4d16420ff82b4f035
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2026-02-15 13:19:10 +0530

    Time: 39 ms (20.61%) | Memory: 44.5 MB (12.84%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    int MOD=INT_MAX;
+
     int helper(int i, int j, string &s, string &t, vector<vector<int>> &dp){
         int n=s.size(),m=t.size();
         if(j>=m) return 1;
@@ -21,7 +23,20 @@ public:
     int numDistinct(string s, string t) {
         int n=s.size(),m=t.size();
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return helper(0,0,s,t,dp);
+        vector<vector<long long>> dp(n+1,vector<long long>(m+1,0));
+
+        for(int i=0; i<=n; i++) dp[i][0]=1;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=(0ll+dp[i-1][j-1]%MOD+dp[i-1][j]%MOD)%MOD;
+                }
+                else dp[i][j]=dp[i-1][j]%MOD;
+            }
+        }
+
+
+        return dp[n][m];
     }
 };
