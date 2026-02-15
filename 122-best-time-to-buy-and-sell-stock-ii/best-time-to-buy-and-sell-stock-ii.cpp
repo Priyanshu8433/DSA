35a153f8b1818e15e27f6cb1cc570aea6e51b867
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2026-02-15 15:56:05 +0530

    Time: 7 ms (22.66%) | Memory: 24.6 MB (9.42%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
-    int helper(int idx, vector<int> &prices, bool flag, vector<vector<int>> &dp){
-        int n=prices.size();
-        if(idx>=n) return 0;
-
-        if(dp[idx][flag]!=-1) return dp[idx][flag];
-
-        // flag==true --> can buy share
-
-
-        if(flag){
-            int buy=helper(idx+1,prices,false,dp)-prices[idx];
-            int notBuy=helper(idx+1,prices,true,dp);
-
-            return dp[idx][flag]=max(buy,notBuy);
-        }
-        else{
-            int sell=helper(idx+1,prices,true,dp)+prices[idx];
-            int notSell=helper(idx+1,prices,false,dp);
-
-            return dp[idx][flag]=max(sell,notSell);
-        }
-
-        return 0;
-    }
-
     int maxProfit(vector<int>& prices) {
         vector<vector<int>> dp(prices.size()+1, vector<int>(2,0));
+        pair<int,int> prev={0,-1e9},curr;
+
         dp[0][1]=-1e9;
 
         for(int i=1; i<=prices.size(); i++){
-            dp[i][1]=max(dp[i-1][0]-prices[i-1], dp[i-1][1]);
-            dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i-1]);
+            curr.second=max(prev.first-prices[i-1], prev.second);
+            curr.first=max(prev.first,prev.second+prices[i-1]);
+
+            prev=curr;
         }
 
-        return dp[prices.size()][0]; 
+        return prev.first; 
     }
 };
