76fc8fb56f49c6803290d45ddf847d48aec2401f
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2026-02-15 12:20:54 +0530

    Time: 27 ms (37.76%) | Memory: 26.9 MB (42.71%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -18,47 +18,35 @@ public:
         }
 
         string lcs="";
+        string ans="";
         int i=n,j=m;
 
         while(i>0 && j>0){
             if(str1[i-1]==str2[j-1]){
-                lcs+=str1[i-1];
+                ans+=str1[i-1];
                 i--;
                 j--;
             }
             else if(dp[i-1][j]>dp[i][j-1]){
+                ans+=str1[i-1];
                 i--;
             }
-            else j--;
+            else{
+                ans+=str2[j-1];
+                j--;
+            }
         }
 
-        reverse(lcs.begin(),lcs.end());
-        
-        i=0;
-        j=0;
-        int k=0;
-
-        string ans="";
-
-        while(i<n && j<m && k<lcs.size()){
-            while(i<n && str1[i]!=lcs[k]){
-                ans+=str1[i];
-                i++;
-            }
-
-            while(j<m && str2[j]!=lcs[k]){
-                ans+=str2[j];
-                j++;
-            }
-
-            ans+=lcs[k];
-            k++;
-            i++;
-            j++;
+        while(i>0){
+            ans+=str1[i-1];
+            i--;
+        }
+        while(j>0){
+            ans+=str2[j-1];
+            j--;
         }
 
-        while(i<n) ans+=str1[i++];
-        while(j<m) ans+=str2[j++];
+        reverse(ans.begin(),ans.end());
 
         return ans;
     }
