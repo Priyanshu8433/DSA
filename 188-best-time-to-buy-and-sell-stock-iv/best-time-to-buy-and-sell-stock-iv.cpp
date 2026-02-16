1e221df6a4541dcdebef11d2ee8e9313e6da0bac
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2026-02-16 12:59:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (88.37%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
-        vector<vector<int>> dp(prices.size()+1,vector<int>(2*k+1,0));
+        vector<int> prev(2*k+1,0),curr(2*k+1,0);
 
-        for(int t=1; t<=2*k; t+=2) dp[0][t]=-1e9;
+        for(int t=1; t<=2*k; t+=2) prev[t]=-1e9;
 
         for(int i=1; i<=prices.size(); i++){
             for(int t=1; t<=2*k; t++){
                 if(t%2==1){
-                    dp[i][t]=max(dp[i-1][t-1]-prices[i-1],dp[i-1][t]);
+                    curr[t]=max(prev[t-1]-prices[i-1],prev[t]);
                 }
                 else{
-                    dp[i][t]=max(dp[i-1][t-1]+prices[i-1],dp[i-1][t]);
+                    curr[t]=max(prev[t-1]+prices[i-1],prev[t]);
                 }
             }
+            prev=curr;
         }
 
-        return dp[prices.size()][2*k];
+        return prev[2*k];
     }
 };
