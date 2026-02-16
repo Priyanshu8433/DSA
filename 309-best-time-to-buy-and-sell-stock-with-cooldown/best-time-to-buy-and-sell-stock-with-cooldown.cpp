6abcc88d8dad1ce5dc607823863c781abeb3bffb
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2026-02-16 13:17:56 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (34.99%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    int helper(int idx, int canBuy, int cd, vector<int> &prices, vector<vector<vector<int>>> &dp){
+    int helper(int idx, int canBuy,vector<int> &prices, vector<vector<int>> &dp){
         int n=prices.size();
         if(idx>=n) return 0;
 
-        if(dp[idx][canBuy][cd]!=-1) return dp[idx][canBuy][cd];
-
-        if(cd==0){
-            if(canBuy){
-                int buy=helper(idx+1,0,0,prices,dp)-prices[idx];
-                int notBuy=helper(idx+1,1,0,prices,dp);
-                return dp[idx][canBuy][cd]=max(buy,notBuy);
-            }
-            else{
-                int sell=helper(idx+1,1,1,prices,dp)+prices[idx];
-                int notSell=helper(idx+1,0,0,prices,dp);
-
-                return dp[idx][canBuy][cd]=max(sell,notSell);
-            }
+        if(dp[idx][canBuy]!=-1) return dp[idx][canBuy];
+        if(canBuy){
+            int buy=helper(idx+1,0,prices,dp)-prices[idx];
+            int notBuy=helper(idx+1,1,prices,dp);
+            return dp[idx][canBuy]=max(buy,notBuy);
         }
         else{
-            return dp[idx][canBuy][cd]=helper(idx+1,1,0,prices,dp);
+            int sell=helper(idx+2,1,prices,dp)+prices[idx];
+            int notSell=helper(idx+1,0,prices,dp);
+
+            return dp[idx][canBuy]=max(sell,notSell);
         }
         return 0;
     }
 
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(2,-1)));
-        return helper(0,1,0,prices,dp);
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return helper(0,1,prices,dp);
     }
 };
